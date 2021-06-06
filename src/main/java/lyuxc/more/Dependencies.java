eeff02cddaa100046b69e2a55ed92f16f3aa4d67
lyuxc-unknow <2675796669@qq.com>
2021-06-06 16:19:35 +0800

    tweak

---
+++ src/main/java/lyuxc/more/Dependencies.java
@@ -0,0 +1,17 @@
+package lyuxc.more;
+
+public class Dependencies {
+    public static final String DEPENDENCIES = "after:torcherino;" +
+            "required-after:theoneprobe;" +
+            "after:extrautils2;" +
+            "after:embers;" +
+            "after:botanicadds;" +
+            "after:botania_tweaks;"+
+            "after:omegacraft;"+
+            "after:lordcraft;"+
+            "after:torcherino;"+
+            "after:soot;"+
+            "after:astralsorcery;"+
+            "after:chancecubes;"+
+            "after:bloodmagic";
+}
